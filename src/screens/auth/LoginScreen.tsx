661baca1ec39d2672574f30315f534b3186739ee
Park YuongHo <hexdrinker2020@gmail.com>
2025-01-29 16:30:55 +0900

    styling login screen

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,11 +1,66 @@
-import {Text, View} from 'react-native';
+import {useState} from 'react';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import InputField from '~/components/InputField';
 
 const LoginScreen = () => {
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [touched, setTouched] = useState({
+    email: false,
+    password: false,
+  });
+
+  const handleChangeValues = (name: string, text: string) => {
+    setValues({
+      ...values,
+      [name]: text,
+    });
+  };
+
+  const handleBlur = (name: string) => {
+    setTouched({
+      ...touched,
+      [name]: true,
+    });
+  };
+
   return (
-    <View>
-      <Text>로그인 화면</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="이메일"
+          error={'이메일을 입력하세요.'}
+          inputMode="email"
+          touched={touched.email}
+          value={values.email}
+          onChangeText={text => handleChangeValues('email', text)}
+          onBlur={() => handleBlur('email')}
+        />
+        <InputField
+          placeholder="비밀번호"
+          error={'비밀번호를 입력하세요.'}
+          secureTextEntry
+          touched={touched.password}
+          value={values.password}
+          onChangeText={text => handleChangeValues('password', text)}
+          onBlur={() => handleBlur('password')}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+  },
+});
 
 export default LoginScreen;
