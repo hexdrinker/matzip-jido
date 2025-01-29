34c4dbca12341ec3e0d2ebd3c3e464381fbf1434
Park YuongHo <hexdrinker2020@gmail.com>
2025-01-29 16:30:46 +0900

    add input field component

---
+++ src/components/InputField.tsx
@@ -0,0 +1,82 @@
+import {useRef} from 'react';
+import {
+  Dimensions,
+  StyleSheet,
+  TextInput,
+  View,
+  TextInputProps,
+  Text,
+  Pressable,
+} from 'react-native';
+import {colors} from '~/constants';
+
+interface InputFieldProps extends TextInputProps {
+  disabled?: boolean;
+  touched?: boolean;
+  error?: string;
+}
+
+const deviceHeight = Dimensions.get('screen').height;
+
+const InputField = ({
+  disabled = false,
+  touched,
+  error,
+  ...props
+}: InputFieldProps) => {
+  const innerRef = useRef<TextInput | null>(null);
+
+  const handlePressInput = () => {
+    innerRef.current?.focus();
+  };
+  return (
+    <Pressable onPress={handlePressInput}>
+      <View
+        style={[
+          styles.container,
+          disabled && styles.disabled,
+          touched && !!error && styles.inputError,
+        ]}>
+        <TextInput
+          ref={innerRef}
+          editable={!disabled}
+          placeholderTextColor={colors.GRAY_500}
+          style={[styles.input, disabled && styles.disabled]}
+          autoCapitalize="none" // 자동 대문자 방지
+          spellCheck={false}
+          autoCorrect={false}
+          {...props}
+        />
+        {touched && !!error && <Text style={styles.errorText}>{error}</Text>}
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: colors.GRAY_200,
+    padding: deviceHeight > 700 ? 15 : 10,
+  },
+  input: {
+    fontSize: 16,
+    color: colors.BLACK,
+    padding: 0,
+  },
+  disabled: {
+    backgroundColor: colors.GRAY_200,
+    color: colors.GRAY_700,
+  },
+  inputError: {
+    borderWidth: 1,
+    borderColor: colors.RED_300,
+  },
+  errorText: {
+    color: colors.RED_500,
+    fontSize: 12,
+    paddingTop: 5,
+  },
+});
+
+export default InputField;
