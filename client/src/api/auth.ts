8fd7b51b5a7078c71a3326797f7e696aba9cde77
Park YuongHo <hexdrinker2020@gmail.com>
2025-02-05 22:39:11 +0900

    add auth api

---
+++ client/src/api/auth.ts
@@ -0,0 +1,56 @@
+import {Category, Profile} from '~/types/domain';
+import axiosInstance from './axios';
+import EncryptedStorage from 'react-native-encrypted-storage';
+import {getEncryptStorage} from '~/utils';
+
+type RequestUser = {
+  email: string;
+  password: string;
+};
+
+const postSignup = async ({email, password}: RequestUser): Promise<void> => {
+  const {data} = await axiosInstance.post('/auth/signup', {email, password});
+
+  return data;
+};
+
+type ResponseToken = {
+  accessToken: string;
+  refreshToken: string;
+};
+
+const postLogin = async ({
+  email,
+  password,
+}: RequestUser): Promise<ResponseToken> => {
+  const {data} = await axiosInstance.post('/login', {email, password});
+
+  return data;
+};
+
+type ResponseProfile = Profile & Category;
+
+const getProfile = async (): Promise<ResponseProfile> => {
+  const {data} = await axiosInstance.get('/auth/me');
+
+  return data;
+};
+
+const getAccessToken = async (): Promise<ResponseToken> => {
+  const refreshToken = await getEncryptStorage('refreshToken');
+  const {data} = await axiosInstance.get('/auth/refresh', {
+    headers: {
+      Authorization: `Bearer ${refreshToken}`,
+    },
+  });
+
+  return data;
+};
+
+const logout = async () => {
+  await axiosInstance.post('/logout');
+};
+
+export {postSignup, postLogin, getProfile, getAccessToken, logout};
+
+export type {RequestUser, ResponseProfile, ResponseToken};
